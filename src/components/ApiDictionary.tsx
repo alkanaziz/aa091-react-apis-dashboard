6e4f2adab333533d97f27f5cba1a49c36e4856b2
alkanaziz <alkanaziz@gmail.com>
2024-03-22 09:50:03 +0100

    click button works

--- src/components/ApiDictionary.tsx
+++ src/components/ApiDictionary.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en'
+
 const ApiDictionary = () => {
-  return (
-    <div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
-        <input type="text" />
-        <button>Get Definition</button>
-        <p>Definition: </p>
-    </div>
-  )
-}
-export default ApiDictionary
+	const [word, setWord] = useState("");
+
+    const handleGetDefinition = () => {
+        alert("clicked")
+    }
+	return (
+		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
+			<input value={word} onChange={(e) => setWord(e.target.value)} type="text" />
+			<button onClick={handleGetDefinition}>Get Definition</button>
+			<p>Definition: </p>
+		</div>
+	);
+};
+export default ApiDictionary;
