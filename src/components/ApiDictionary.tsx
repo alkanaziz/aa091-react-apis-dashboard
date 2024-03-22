456659008a6406d5f22346eceff344ac83ef1f1a
alkanaziz <alkanaziz@gmail.com>
2024-03-22 10:28:49 +0100

    Refactor ApiDictionary component and update button styling

--- src/components/ApiDictionary.tsx
+++ src/components/ApiDictionary.tsx
@@ -5,28 +5,30 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
 
 const ApiDictionary = () => {
 	const [word, setWord] = useState("");
+	const [definition, setDefinition] = useState("");
 
-	const handleGetDefinition = (e:React.MouseEvent<HTMLButtonElement>) => {
+	const handleGetDefinition = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault();
-        
-        (async () => {
-            const response = await axios.get(`${url}/house`)
-            const data = response.data;
-            console.log(data);
-        })();
-    };
+
+		(async () => {
+			const response = await axios.get(`${url}/${word}`);
+			const data = response.data;
+			const _definition = data[0].meanings[0].definitions[0].definition;
+			setDefinition(_definition);
+		})();
+	};
 
 	return (
-		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
-			<form>
-				<input
-					value={word}
-					onChange={(e) => setWord(e.target.value)}
-				/>
-				<button onClick={(e) => handleGetDefinition(e)}>Get Definition</button>
+		<form>
+			<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
+				<input value={word} onChange={(e) => setWord(e.target.value)} />
+				<button onClick={(e) => handleGetDefinition(e)}>
+					Get Definition
+				</button>
 				<p>Definition: </p>
-			</form>
-		</div>
+				<p>{definition}</p>
+			</div>
+		</form>
 	);
 };
 export default ApiDictionary;
