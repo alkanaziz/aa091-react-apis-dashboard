abce0856113f8c5f72858b0430316da38bc2631f
alkanaziz <alkanaziz@gmail.com>
2024-03-22 10:08:39 +0100

    getting data with function

--- src/components/ApiDictionary.tsx
+++ src/components/ApiDictionary.tsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
+import axios from "axios";
 
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en'
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en";
 
 const ApiDictionary = () => {
 	const [word, setWord] = useState("");
 
-    const handleGetDefinition = () => {
-        alert("clicked")
-    }
+	const handleGetDefinition = (e:React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+        
+        (async () => {
+            const response = await axios.get(`${url}/house`)
+            const data = response.data;
+            console.log(data);
+        })();
+    };
+
 	return (
 		<div className="flex gap-5 flex-col bg-slate-200 p-6 w-[20rem] rounded">
-			<input value={word} onChange={(e) => setWord(e.target.value)} type="text" />
-			<button onClick={handleGetDefinition}>Get Definition</button>
-			<p>Definition: </p>
+			<form>
+				<input
+					value={word}
+					onChange={(e) => setWord(e.target.value)}
+				/>
+				<button onClick={(e) => handleGetDefinition(e)}>Get Definition</button>
+				<p>Definition: </p>
+			</form>
 		</div>
 	);
 };
